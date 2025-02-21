e3979997aaefd4cad4363be8034da8c6b113c905
ilorez <zobirnajdaoui@gmail.com>
2025-02-21 12:04:02 +0100

    sleep but why it's doesn't work ??

--- philo/srcs/philo_life_cycle.c
+++ philo/srcs/philo_life_cycle.c
@@ -6,7 +6,7 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 14:58:38 by znajdaou          #+#    #+#             */
-/*   Updated: 2025/02/20 19:41:25 by znajdaou         ###   ########.fr       */
+/*   Updated: 2025/02/21 12:02:42 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,16 @@ void *ft_philo_life_cycle(void *ptr)
   ft_change_time(&(p->start_time), p->lstart_time);
   while (!p->data->limited || ++p->eats <= p->data->max_eats)
   {
+    printf("id is ->%u\n", p->id);
+    if (p->id % 2 == 0)
+      usleep(p->data->teat);
     pthread_mutex_lock((p->data->forks[p->id -1]));
     ft_print_msg_status(p);
     pthread_mutex_lock((p->data->forks[p->id % p->data->philo_num]));
     ft_print_msg_status(p);
+    printf("change status\n");
     ft_change_status(p->data, p, EATING);
+    printf("changed\n");
     usleep(p->data->teat);
     pthread_mutex_unlock((p->data->forks[p->id % p->data->philo_num]));
     pthread_mutex_unlock((p->data->forks[p->id -1]));
@@ -56,12 +61,18 @@ void ft_change_time(time_t *var, pthread_mutex_t lock)
 
 void ft_change_status(t_data *data, t_philo *philo, t_pstatus to)
 {
+
+
+  printf("try to lock status in change status\n");
   pthread_mutex_lock(&(philo->lstatus));
   philo->status = to;
   pthread_mutex_unlock(&(philo->lstatus));
+  printf("unlocked status in change status\n");
+  printf("try to lock lis_done in change status\n");
   pthread_mutex_lock(&(data->lis_done));
   if (!data->is_done)
     ft_print_msg_status(philo);
   pthread_mutex_unlock(&(data->lis_done));
+  printf("unlocked is_done in change status\n");
   return ;
 }
