b0b2703f4d2633228f0dcb95134783eb24244bd7
ilorez <zobirnajdaoui@gmail.com>
2025-03-03 12:52:05 +0000

    in the middle of creating behavior of philo life

--- philo_bonus/srcs/philos_creature.c
+++ philo_bonus/srcs/philos_creature.c
@@ -6,12 +6,48 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 12:06:38 by znajdaou          #+#    #+#             */
-/*   Updated: 2025/03/03 12:34:18 by znajdaou         ###   ########.fr       */
+/*   Updated: 2025/03/03 12:51:38 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+/*
+* typedef struct s_philo
+{
+	unsigned int	id;
+	int				eats;
+	time_t			start_time;
+	t_pstatus		status;
+	pthread_t		thr;
+	t_bool			is_done;
+	pthread_mutex_t	lis_done;
+	pthread_mutex_t	lstart_time;
+	pthread_mutex_t	lstatus;
+	t_data			*data;
+}					t_philo;
+*/
+
+// INFO:---------------------------
+t_philo ft_philo_init(int id, t_data *data)
+{
+  t_philo *p;
+
+  p = ft_calloc(sizeof(t_philo), 1);
+  if (!p)
+    //TODO:
+  p->id = id;
+  p->data = data;
+  p->start_time = data->start_time + data->philo_num * 20;
+
+
+
+
+
+}
+ 
+
+// --------------------------------
 
 t_errno ft_philos_creature(t_data *d)
 {
@@ -29,7 +65,11 @@ t_errno ft_philos_creature(t_data *d)
     }
     else if (d->pid[i] == 0)
     {
+      free(d->pid)
       printf("Hello! from child %d\n", i+1);
+      // TODO: init philo data
+      // TODO: create philo cyrcle life
+      // TODO: free philo
       exit(0);
     }
   }
